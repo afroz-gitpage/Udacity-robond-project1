1224a418dbfdd212d1183c75e231bacce5512b44
Afroz Mohammed <to.afrozkhan@gmail.com>
2020-04-30 15:20:07 -0700

    First commit

---
+++ myrobot/script/welcome_message.cpp
@@ -0,0 +1,20 @@
+#include <gazebo/gazebo.hh> //directories pointer
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin //Creating a class from world plugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to Shark’s World!!\n"); // printing out the message
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)//Loading the physics engine
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot) //Registering the plugin
+}
+
+
+
